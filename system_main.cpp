b21040bc21e4bde07447537219dd467802653743
李呈祥 <624786053@qq.com>
2020-01-21 11:51:15 +0800

    test(): use CPP light LED,c++ test is OK

---
+++ system_main.cpp
@@ -0,0 +1,36 @@
+/*
+ * system_main.cpp
+ *
+ *  Created on: Jan 21, 2020
+ *      Author: lcx
+ */
+
+
+
+#include "led.h"
+#include "tick.h"
+#include "sys.h"
+
+
+
+int main(void)
+{
+
+    HAL_Init();
+    Stm32_Clock_Init(360,25,2,8);
+    tick_delay_init(180);
+    LED_Init();
+
+
+
+	while(1)
+	{
+
+		LED0 = !LED0;
+		LED1 = !LED1;
+		delay_ms(500);
+
+	}
+	return 0;
+}
+
